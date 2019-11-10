df5c0367eb605b369f06151c02fc08fad3c112c3
sam9494 <samchong9494@gmail.com>
2019-11-10 16:18:26 +0800

    copy Game from another project

---
+++ TicTAcToe/TicTAcToe/GameTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using TicTacToe;
+
+namespace TicTacToe
+{
+    public class GameTest
+    {
+        [Test]
+        public void testOnlyAvailableMove()
+        {
+            var game = new Game("XOXOX-OXO");
+            Assert.AreEqual(5, game.Move('X'));
+
+            game = new Game("XOXOXOOX-");
+            Assert.AreEqual(8, game.Move('O'));
+        }
+
+        [Test]
+        public void testStartingDefaultMove()
+        {
+            Game game = new Game("---------");
+            Assert.AreEqual(0, game.Move('X'));
+        }
+
+        [Test]
+        public void testNoAvailableMove()
+        {
+            Game game = new Game("XXXXXXXXX");
+            Assert.AreEqual(-1, game.Move('X'));
+        }
+
+        [Test]
+        public void testFindWinningRowMove()
+        {
+            Game game = new Game("OO-XX-OOX");
+            Assert.AreEqual(5, game.Move('X'));
+        }
+
+        [Test]
+        public void testWinByRowConditions()
+        {
+            Game game = new Game("---XXX---");
+            Assert.AreEqual('X', game.Winner());
+
+            game = new Game("------OOO");
+            Assert.AreEqual('O', game.Winner());
+
+            game = new Game("YYY------");
+            Assert.AreEqual('Y', game.Winner());
+        }
+    }
+}
